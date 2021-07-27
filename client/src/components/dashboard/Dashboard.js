a679067b333fc7d31db03b6fcb13b03a2c277e4d
Chawput <chawput@gmail.com>
2021-07-27 16:56:55 +0700

    44. Protect Route for Dashbaord (PrivateRoute)

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+// racfp
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = props => {
+	return <div>Dashboard</div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
