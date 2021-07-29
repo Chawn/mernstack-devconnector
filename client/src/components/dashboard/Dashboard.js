bf40a751b3355ecf55cd1a075e082b8406e89893
Chawput <chawput@gmail.com>
2021-07-29 16:10:47 +0700

    46. Starting on the Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,39 @@
 // racfp
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+	getCurrentProfile,
+	auth: { user },
+	profile: { profile, loading },
+}) => {
 	useEffect(() => {
 		getCurrentProfile();
 	}, []);
-	return <div>Dashboard</div>;
+	return loading && profile === null ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<h1 className='large text-primary'>Dashboard</h1>
+			<p className='lead'>
+				<i className='fas fa-user'></i> Welcome {user && user.name}
+			</p>
+			{profile !== null ? (
+				<Fragment>has</Fragment>
+			) : (
+				<Fragment>
+					<p>คุณยังม่ได้ตั้งค่าโปรไฟล์ กรุณาใส่ข้อมูลโปรไฟล์ของคุณ</p>
+					<Link to='/create-profile' class='btn btn-primary my-1'>
+						Create Profile
+					</Link>
+				</Fragment>
+			)}
+		</Fragment>
+	);
 };
 
 Dashboard.propTypes = {
